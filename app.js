9dbdfe4a699e50a79dcd3f7dca3c83c5f16c4425
dp <developerdeloar@gmail.com>
2022-11-28 13:40:12 +0600

    Today I practices Variable, Data Type, For loop and Array

---
+++ app.js
@@ -0,0 +1,44 @@
+// Variable 
+
+var x = 10;
+var y = 54;
+var total = x + y;
+
+let xy = 23;
+let yx = 45;
+let additotal = xy * yx;
+
+const jsPlaying = "JavaScript Jhor Jhoora kore felbo";
+
+console.log(total);
+console.log(additotal);
+console.log(jsPlaying);
+
+
+//Data Type
+
+let age = 18;
+var name = "Deloar";
+let fullname = { first: "Deloar", last: "Hossain" };
+const truth = false;
+var sheets = ["HTML", "CSS", "JavaScript", "PHP", "Laravel"];
+let a = null;
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(fullname));
+console.log(typeof(truth));
+console.log(typeof(sheets));
+console.log(typeof(a));
+
+
+//Foor Loop
+for (let i = 0; i < 30; i++) {
+    console.log(i);
+}
+
+
+//Array
+const myArray = ["Cycle", "Bike", "BMW"];
+
+console.log(myArray[0, 2]);
